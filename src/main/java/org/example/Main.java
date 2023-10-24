01c62798f77785bd6867504ac6b8142be65c7d17
Bizykova <bizykova.daria@gmail.com>
2023-10-24 19:26:56 +0300

    создать конфликт

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Scanner a = new Scanner(System.in);
+        System.out.println("\n Enter the number");
+        int num = a.nextInt();
+        System.out.println("\n You have entered a number " + num);
     }
 }
