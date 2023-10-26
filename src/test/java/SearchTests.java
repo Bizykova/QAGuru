535586de3a6dccdbcb3f6cb9ab227ce6c8d4af5c
Bizykova <bizykova.daria@gmail.com>
2023-10-26 22:55:43 +0300

    remove timeout

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -9,7 +9,6 @@ public class SearchTests {
 
     @Test
     void successfulSearchTest() {
-        Configuration.timeout = 50000;
         open("https://www.google.com/");
         $("[name=q]").setValue("Selenide").pressEnter();
         $("[id=search]").shouldHave(Condition.text("selenide.org"));
